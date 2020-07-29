d3f5a3520cb182ce53aa6efa94b0736cee68a067
Mteixeira88 <mteixeira88013@gmail.com>
2020-07-29 12:51:58 +0100

    Initial Commit

---
+++ Second-Wallet/Second_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_WalletApp.swift
+//  Second-Wallet
+//
+//  Created by Miguel Teixeira on 29/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct Second_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
